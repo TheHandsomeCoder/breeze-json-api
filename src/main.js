92d29297a30f19b72ab916e5df8422c809554b69
Scott O'Malley <scottyomalley@gmail.com>
2016-05-23 10:43:01 +0100

    Added new test syntax

--- src/main.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by somalley on 18/05/2016.
- */
--- test/test.js
+++ test/test.js
@@ -1,8 +1,47 @@
-describe('Array', function() {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        });
-    });
+describe('breeze-json-api-uribuilder', function() {
+
+	let uriBuilder = {};
+	let manager = {};
+
+	let executeQuery = function(query){
+		return uriBuilder.buildUri(query, manager.metadataStore)
+	}
+
+
+	before(function() {
+		breeze.config.initializeAdapterInstances({
+			uriBuilder: "json-api"
+		});
+		const ds = new breeze.DataService({
+			serviceName: 'http://localhost:9000',
+			hasServerMetadata: false
+		});
+
+		manager = new breeze.EntityManager({
+			dataService: ds
+		});
+
+		uriBuilder = breeze.config.getAdapterInstance("uriBuilder", 'json-api');
+	});
+
+	describe('breeze.EntityQuery().from("persons")', function() {
+		it('should return the URI for the persons endpoint', function() {
+
+			const expectedURI = 'persons';
+			const query = new breeze.EntityQuery().from("persons");
+
+			expect(executeQuery(query), 'Get all Persons').equal(expectedURI);			
+		});
+	});
+
+	describe('breeze.EntityQuery().from("persons").where("ID", "==", "12345");', function() {
+		it('should return the URI for a specific person', function() {
+
+			const expectedURI = 'persons/12345';
+			const query = new breeze.EntityQuery().from("persons").where("ID", "==", "12345");
+
+			expect(executeQuery(query), "Get specific person").equal(expectedURI);					
+		});
+	});
+
 });
